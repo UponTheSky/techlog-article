1191f49c69e8fef0a12228bc1fa043a58726c08c
UponTheSky <sailing.to.orbis@gmail.com>
2022-11-20 00:31:51 +0900

    use separate .env files

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,23 +2,4 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const NODE_ENV = process.env.NODE_ENV || 'development';
-
-export let DATABASE_URL: string;
-
-switch (NODE_ENV) {
-  case 'development':
-    DATABASE_URL = process.env.DEV_DATABASE_URL as string;
-    break;
-
-  case 'test':
-    DATABASE_URL = process.env.TEST_DATABASE_URL as string;
-    break;
-
-  case 'production':
-    DATABASE_URL = process.env.PROD_DATABASE_URL as string;
-    break;
-
-  default:
-    break;
-}
+export const DATABASE_URL = process.env.DATABASE_URL as string;
