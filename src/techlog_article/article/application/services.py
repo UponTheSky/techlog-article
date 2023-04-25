f89c43c663f477cc3d269269e37ef0c68c2d2f4c
UponTheSky <sailing.to.orbis@gmail.com>
2023-04-25 11:18:29 +0900

    append: DI on the service class

--- src/techlog_article/article/application/services.py
+++ src/techlog_article/article/application/services.py
@@ -1,12 +1,20 @@
-from typing import final
+from typing import final, Annotated
+
+from fastapi import Depends
 
 from .port.in_ import CreateArticleInDTO, CreateArticleInPort
 from .port.out import CreateArticleOutDTO, CreateArticleOutPort
+from adapter.out.persistences import ArticleUserPersistenceAdapter
 
 
 @final
 class CreateArticleService(CreateArticleInPort):
-    def __init__(self, create_article_out_port: CreateArticleOutPort):  # TODO: DI
+    def __init__(
+        self,
+        create_article_out_port: Annotated[
+            CreateArticleOutPort, Depends(ArticleUserPersistenceAdapter)
+        ],
+    ):
         self._create_article_out_port = create_article_out_port
 
     async def create_article(self, *, dto: CreateArticleInDTO) -> None:
