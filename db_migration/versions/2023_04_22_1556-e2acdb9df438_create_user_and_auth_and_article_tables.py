f8d4c7b9d49323b701fc7a2ed0c5a1aebfed1023
UponTheSky <sailing.to.orbis@gmail.com>
2023-04-25 14:27:17 +0900

    db migration(#41)

--- db_migration/versions/2023_04_22_1556-e2acdb9df438_create_user_and_auth_and_article_tables.py
+++ db_migration/versions/2023_04_22_1556-e2acdb9df438_create_user_and_auth_and_article_tables.py
@@ -1,4 +1,4 @@
-"""create_user_and_auth_tables
+"""create_user_and_auth_and_article_tables
 
 Revision ID: e2acdb9df438
 Revises: cc40bcb48d81
@@ -22,15 +22,27 @@ def upgrade() -> None:
         "user",
         sa.Column("id", sa.UUID, primary_key=True),
         sa.Column("username", sa.String(16), index=True),
+        sa.Column("email", sa.String, index=True),
+        sa.Column("hashed_password", sa.String),
     )
 
     op.create_table(
         "auth",
         sa.Column("id", sa.UUID, primary_key=True),
-        sa.Column("user_id", sa.UUID, sa.ForeignKey("user.id")),
+        sa.Column("user_id", sa.UUID, sa.ForeignKey("user.id"), index=True),
         sa.Column("access_token", sa.String, nullable=True),
+    )
+
+    op.create_table(
+        "article",
+        sa.Column("id", sa.UUID, primary_key=True),
+        sa.Column("author_id", sa.UUID, sa.ForeignKey("user.id"), index=True),
+        sa.Column("title", sa.String(32)),
+        sa.Column("content", sa.Text, nullable=True),
     )
 
 
 def downgrade() -> None:
+    op.drop_table("user")
     op.drop_table("auth")
+    op.drop_table("article")
