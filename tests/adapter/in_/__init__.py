1793958ab7633d43e64c64cbeef66f4420d95c33
UponTheSky <sailing.to.orbis@gmail.com>
2023-05-01 23:49:26 +0900

    add: article web adapter test

--- tests/adapter/in_/__init__.py
+++ tests/adapter/in_/__init__.py
@@ -1,6 +1,8 @@
-from src.techlog_article.user.adapter.in_.controllers import *  # noqa: F403
 from src.techlog_article.server import app  # noqa: F401
 from src.techlog_article.auth import CurrentUserIdDependency  # noqa: F401
 from src.techlog_article.auth.application.services import (
     AuthTokenCheckServiceDependency,  # noqa: F401
 )
+
+from src.techlog_article.user.adapter.in_.controllers import *  # noqa: F403
+from src.techlog_article.article.adapter.in_.controllers import *  # noqa: F403
