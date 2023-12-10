fe26756dd77186faacbbe14b86181aaf1ea8a085
UponTheSky <sailing.to.orbis@gmail.com>
2023-12-11 07:32:25 +0900

    add: logging middleware

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,7 +5,3 @@ from techlog_article.common.database import utils
 # this is for mocking the decorators
 # for details: https://www.freblogg.com/pytest-functions-mocking-1
 mock.patch.object(utils, "transactional", lambda x: x).start()
-
-# this is for mocking Mangum
-# since as it wraps the app object we lose the interface for the controller tests
-mock.patch("mangum.Mangum", lambda x: x).start()
